17fadb7feb0b8c15e4b5e1be6b46dc20564bb16c
Max Cardwell <maximillian.cardwell@gmail.com>
2022-04-15 12:38:20 +0100

    Made it so you cannot divide by 0

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -9,11 +9,66 @@ describe("Calculator", () => {
     cy.get('.display').should('contain', '2')
   });
 
-  // it('should be able to add 1 and 4', () => {
-  //   cy.get('#number1').click();
-  //   cy.get('#operator-add').click();
-  //   cy.get('#number4').click();
-  //   cy.get('#operator-equals').click();
-  //   const result
-  // })
+  it('should show the result of arithmetic operations', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_add').click();
+    cy.get('#number1').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '2');
+  });
+
+  it('should be able to chain multiple functions together', () => {
+    cy.get('#number1').click();
+    cy.get('#operator_add').click();
+    cy.get('#number1').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number4').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '8');
+  });
+
+  it('should show decimal numbers', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '1.5');
+  });
+
+  it('should show large numbers', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number7').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number7').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number7').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '1029');
+  });
+
+  it('should show positive numbers', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '4');
+  });
+
+  it('should show negative numbers', () => {
+    cy.get('#number2').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number4').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-2');
+  });
+
+  it('should send an alert if you try to divide by 0', () => {
+    cy.get('#number2').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', 'You cannot divide by 0')
+  });
+
 })
